eb7085c2487b5f72614fc5311b9add7a5cc721d0
sudodeepak <sudodeepak@gmail.com>
2022-08-27 13:41:49 +0530

    #1

---
+++ src/pages/_app.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import '../styles/globals.css'
+import NavBar from 'components/NavBar'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+    <NavBar />
+    <Component {...pageProps} />
+    </>
+  ) 
+}
+
+export default MyApp
---
+++ src/pages/_document.jsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet" /> 
+        </Head>
+        <body>
+        <div className="page_width">
+          <Main />
+        
+          <NextScript />  </div>
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
